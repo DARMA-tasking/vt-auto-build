b9ce47bd7e270c7bc4895782da774f705f69bbc8
Jonathan Lifflander <jliffla@sandia.gov>
2018-11-05 09:51:54 -0800

    add missing file

---
+++ build-kokkos.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if test $# -lt 2
+then
+    echo "usage $0 <build-mode>"
+    exit 1;
+fi
+
+build_mode=$1
+
+compiler_c=clang-mp-3.9
+compiler_cxx=clang++-mp-3.9
+
+if test $# -gt 1; then compiler_c=$2; fi
+if test $# -gt 2; then compiler_cxx=$3; fi
+
+cmake ../kokkos                                                        \
+      -DCMAKE_INSTALL_PREFIX=../kokkos-install                         \
+      -DCMAKE_CXX_COMPILER=${compiler_cxx}                             \
+      -DCMAKE_C_COMPILER=${compiler_c}                                 \
+      -DCMAKE_BUILD_TYPE=${build_mode}                                 \
+      -DKOKKOS_ENABLE_OPENMP:BOOL=ON                                   \
+      -DCMAKE_EXPORT_COMPILE_COMMANDS=true
